5b82c05c1d1b4d15309536b2021abe50ef8aba56
Bruno <bruneti12@gmail.com>
2022-08-01 20:54:37 -0500

    Inicial de A5 y A6 Implementación de E2 de A5

---
+++ A5_E3/A5_E3/Ejercicio2/Persona.cs
@@ -0,0 +1,16 @@
+﻿namespace A5_E3.Ejercicio2;
+
+public class Persona
+{
+    public static readonly string[] ValoresAceptadosSexo = new[] { "M", "F" };
+    public string Nombre { get; set; }
+    
+    public int Edad { get; set; }
+    
+    public string Sexo { get; set; }
+
+    public override string ToString()
+    {
+        return $"{Nombre} | {Edad} | {Sexo}";
+    }
+}
---
+++ A5_E3/A5_E3/UtilidadesConsola.cs
@@ -0,0 +1,54 @@
+﻿using System.Text.RegularExpressions;
+
+namespace A5_E3.Ejercicio2;
+
+public class UtilidadesConsola
+{
+    public string SolicitarString(string texto, Regex? regex = null, string[]? valoresAceptados = null)
+    {
+        Console.Write(texto);
+        var input = Console.ReadLine();
+
+        bool inputValido = false;
+        while (!inputValido)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                Console.Write(texto);
+                input = Console.ReadLine();
+                continue;
+            }
+                
+            if (regex != null && !regex.IsMatch(input))
+            {
+                Console.Write("Formato inválido!: ");
+                input = Console.ReadLine();
+                continue;
+            }
+
+            if (valoresAceptados != null && !valoresAceptados.Contains(input.ToUpper()))
+            {
+                Console.Write("Ingrese un valor aceptado!: ");
+                input = Console.ReadLine();
+                continue;
+            }
+            
+            inputValido = true;
+        }
+            
+        return input;
+    }
+    
+    public int SolicitarEntero(string texto)
+    {
+        var input = SolicitarString(texto);
+        
+        int entero = 0;
+        if (!string.IsNullOrEmpty(input))
+        {
+            int.TryParse(input, out entero);
+        }
+
+        return entero;
+    }
+}
